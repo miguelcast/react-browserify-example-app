534a052912dd8ce9e2c22fa84eb23e3805660f26
unknown <castrilu@PCG79JKR1.INTERNO.LD>
2016-06-16 16:45:50 -0500

    add USE STRICT

--- src/const/consts.js
+++ src/const/consts.js
@@ -1,3 +1,4 @@
+"use strict";
 module.exports = {
     'path' : '//localhost:63342/react-browserify-example-app/news_mock.json'
 };
--- src/stores/News.js
+++ src/stores/News.js
@@ -1,3 +1,4 @@
+"use strict";
 var Const = require('../const/consts');
 var jQuery = require('jquery');
 
